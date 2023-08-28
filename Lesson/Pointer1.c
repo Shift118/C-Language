ce40a57006eb6ec0ff69ec3fa441a826832017ea
Shift118 <123782957+Shift118@users.noreply.github.com>
2023-08-28 12:46:53 +0300

    Update Pointer1.c

--- Lesson/Pointer1.c
+++ Lesson/Pointer1.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+int increment(int *a){
+    *a++;
+    printf("%d\n", *a);
+}
+
 int main(void) {
     int x =121;
     int *pointerofX = &x;
@@ -7,4 +12,10 @@ int main(void) {
     printf("%d\n",x);
     printf("%d\n",pointerofX);
     printf("%d\n",&x);
+/////////////////////////////////////
+    int arr[] = {2,3,4,5,6,7,8};
+    
+    increment(&arr[2]);
+    
+    printf("%d\n",arr[2]); 
 }
