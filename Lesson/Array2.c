e5de6b04485c425a1ce9b6c82b5e3af0a59e9aea
Shift118 <123782957+Shift118@users.noreply.github.com>
2023-08-28 11:19:05 +0300

    Create Array2.c

---
+++ Lesson/Array2.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int increment(int a){
+    a++;
+    return a;
+}
+
+
+int main(void) {
+    // Write C code here
+    int i , arr[] = {2,3,4,5,6,7,8};
+    
+    printf("Size of Array = %d\n",sizeof(arr)/sizeof(arr[0]));
+    // Printing all Using LOOP
+    for (i = 0; i < sizeof(arr)/sizeof(arr[0]); i++){
+        printf("%d\n",increment(arr[i]));
+    }
+}
